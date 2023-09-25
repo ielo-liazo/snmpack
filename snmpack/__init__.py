697cbc12ea7732901af593dd2c9fcbfd173d0c4b
Rémi Desgrange <remi.desgrange@ielo.net>
2023-09-25 15:18:07 +0200

    Fix: importlib

--- snmpack/__init__.py
+++ snmpack/__init__.py
@@ -1,3 +1,6 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
 
-__version__ = version(__package__)
+    __version__ = version(__package__)
+except:  # noqa: E722
+    __version__ = "cannot be extracted, upgrade to at least 3.8"
